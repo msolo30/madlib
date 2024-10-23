3fdc3b17a4600ebdfe11f643a4e9ea9dd2b1c7b8
Sophia <sophiam2702@tuckahoeschools.org>
2024-10-23 12:30:46 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,24 @@
+adj1 = input("Type an adjective: ")
+adj2 = input("Type an adjective: ")
+adj3 = input("Type an adjective: ")
+food = input("Choose a food: ")
+adj4 = input("Type an adjective: ")
+adverb = input("Type an adverb: ")
+adj5 = input("Type an adjective: ")
+nickname = input("Pick a nickname: ")
+car = input("Choose a car: ")
+adverb2 = input("Type an adverb: ")
+adj6 = input("Type an adjective: ")
+
+print(f"""Once upon a time there was a girl going to school.
+Her school was {adj1}, she hates her school. 
+The {adj2} girl was getting school lunch. It tasted {adj3}
+She had a {food}. It tasted {adj4}. She went to the nurse.
+No one was inside the nurse's office. She {adverb} left.
+She calls her mom. Get me out of this {adj5} school.
+Her mom said "Ok coming {nickname}". Her mom came but when
+she walked into the {car}, it wasn't her mom driving.
+She {adverb2} shut the car door. She looked around
+and noticed the school was deserted. She got dragged into
+the {car}. The woman who picker her up wasn't her {adj6}
+mother. IT WAS UGLY ELIZABETH GJELAJ. """)
